e53f225766454e2275b2cabd129bbf998e97c001
AnirudhHada <hadaanirudh10@gmail.com>
2024-11-30 01:09:52 +0530

    advice_app done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import './App.css';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [advice, setAdvice] = useState('');
+	const [advice, setAdvice] = useState("");
 
-  useEffect(() => {
-    console.log('component did mount');
-  }, [])
+	useEffect(() => {
+		fetchAdvice();
+	}, []);
 
-  const fetchAdvice = () => {
-    axios.get('https://api.adviceslip.com/advice');
-  }
+	const fetchAdvice = () => {
+		axios
+			.get("https://api.adviceslip.com/advice")
+			.then((response) => {
+				const advice = response.data.slip.advice;
+				setAdvice(advice);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 
-  return (
-    <>
-      <h1>App</h1>
-    </>
-  );
+	return (
+		<div className="app">
+			<div className="card">
+				<h1 className="heading">{advice}</h1>
+        <button className="button" onClick={fetchAdvice}>
+          <span>GIVE ME ADVICE!</span>
+        </button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
